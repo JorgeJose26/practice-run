444aeeb2e72bb3eb4c1b118123436358a05df0f8
Jorge Casaudoumecq <Jorge@Casaudoumecq.me>
2023-05-04 14:55:16 -0400

    set up skeleton for home page.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import "./HomePage.scss"
-import NavBar from '../../components/NavBar/NavBar';
-import Card from '../../components/Card/Card';
-import MenuButtons from '../../components/MenuButtons/MenuButtons';
-import { MDBContainer } from 'mdb-react-ui-kit';
+import { useNavigate } from "react-router-dom";
+
 
 
 function HomePage(props) {
+    const navigate = useNavigate();
     return (
-        <div className='Home'>
-            <NavBar />
-            <MDBContainer className='w-25 p-3'>
-                <Card />
-            </MDBContainer>
-            <MenuButtons />
+        <div className='home'>
+            <div className='home-container'>
+                <h2>Welcome to pokemon type tester!</h2>
+                <p>This game will test your pokemon type knowledge</p>
+                <p>The game is simple. A random pokemon will appear before you and the challange is to choose one super effctive
+                    type against the pokemon.
+                </p>
+                <p>Ready to be a pokemon master?</p>
+                <button onClick={() => navigate(`/game`)} >Click Here!</button>
+
+            </div>
+
+
 
 
         </div>
