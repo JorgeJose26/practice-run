4ed49517cfaf4ca9c1eb2c9ff1cb161da34bd45a
Jorge Casaudoumecq <Jorge@Casaudoumecq.me>
2023-05-09 15:44:10 -0400

    added the handle function

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import "./Card.scss";
 import axios from 'axios';
+import { type } from '@testing-library/user-event/dist/type';
 
 
 
@@ -9,11 +10,14 @@ import axios from 'axios';
 function Card(props) {
     const [pokemon, setPokemon] = useState(null);
 
+
     useEffect(() => {
         const getRandomPokemon = async () => {
             const randomNumber = Math.floor(Math.random() * 898) + 1;
             const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
             setPokemon(response.data);
+            console.log(response.data);
+
         };
 
         getRandomPokemon();
@@ -29,6 +33,98 @@ function Card(props) {
             .join(' ');
     };
 
+    const typeMatchups = {
+        normal: {
+            weaknesses: ['fighting'],
+            strengths: [],
+        },
+        fire: {
+            weaknesses: ['water', 'ground', 'rock'],
+            strengths: ['grass', 'ice', 'bug', 'steel'],
+        },
+        water: {
+            weaknesses: ['electric', 'grass'],
+            strengths: ['fire', 'ground', 'rock'],
+        },
+        electric: {
+            weaknesses: ['ground'],
+            strengths: ['water', 'flying'],
+        },
+        grass: {
+            weaknesses: ['fire', 'ice', 'poison', 'flying', 'bug'],
+            strengths: ['water', 'ground', 'rock'],
+        },
+        ice: {
+            weaknesses: ['fire', 'fighting', 'rock', 'steel'],
+            strengths: ['grass', 'ground', 'flying', 'dragon'],
+        },
+        fighting: {
+            weaknesses: ['flying', 'psychic', 'fairy'],
+            strengths: ['normal', 'ice', 'rock', 'dark', 'steel'],
+        },
+        poison: {
+            weaknesses: ['ground', 'psychic'],
+            strengths: ['grass', 'fairy'],
+        },
+        ground: {
+            weaknesses: ['water', 'grass', 'ice'],
+            strengths: ['fire', 'electric', 'poison', 'rock', 'steel'],
+        },
+        flying: {
+            weaknesses: ['electric', 'ice', 'rock'],
+            strengths: ['grass', 'fighting', 'bug'],
+        },
+        psychic: {
+            weaknesses: ['bug', 'ghost', 'dark'],
+            strengths: ['fighting', 'poison'],
+        },
+        bug: {
+            weaknesses: ['fire', 'flying', 'rock'],
+            strengths: ['grass', 'psychic', 'dark'],
+        },
+        rock: {
+            weaknesses: ['water', 'grass', 'fighting', 'ground', 'steel'],
+            strengths: ['fire', 'ice', 'flying', 'bug'],
+        },
+        ghost: {
+            weaknesses: ['ghost', 'dark'],
+            strengths: ['psychic', 'ghost'],
+        },
+        dragon: {
+            weaknesses: ['ice', 'dragon', 'fairy'],
+            strengths: ['dragon'],
+        },
+        dark: {
+            weaknesses: ['fighting', 'bug', 'fairy'],
+            strengths: ['psychic', 'ghost'],
+        },
+        steel: {
+            weaknesses: ['fire', 'fighting', 'ground'],
+            strengths: ['ice', 'rock', 'fairy'],
+        },
+        fairy: {
+            weaknesses: ['poison', 'steel'],
+            strengths: ['fighting', 'dragon', 'dark'],
+        },
+    };
+
+
+
+
+
+    const handleTypeMatchUps = () => {
+        let correctAwnser = [];
+        let incorrectAwnser = "Wrong";
+        if (pokemon.types[0].type.name === 'Normal') {
+            typeMatchups.normal.weaknesses[0] = correctAwnser[0];
+            typeMatchups.normal.weaknesses[1] = correctAwnser[1];
+
+        }
+        else {
+            incorrectAwnser;
+        }
+
+    }
 
 
 
--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -2,12 +2,15 @@ import NavBar from '../../components/NavBar/NavBar';
 import Card from '../../components/Card/Card';
 import MenuButtons from '../../components/MenuButtons/MenuButtons';
 import { MDBContainer } from 'mdb-react-ui-kit';
+
 import React from 'react';
 
 function GamePage(props) {
+
     return (
         <div>
             <NavBar />
+
             <MDBContainer className='w-25 p-3'>
                 <Card />
             </MDBContainer>
